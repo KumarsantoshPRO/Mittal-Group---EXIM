37dce3502127879d7c7d5692ed74ddb6b556ace8
Admin MG <admin@mittalgroup.com>
2024-08-08 12:08:57 +0000

    Elango changes

--- Transactional-Shipping bill/webapp/controller/View1.controller.js
+++ Transactional-Shipping bill/webapp/controller/View1.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
-function (Controller) {
+function (Controller,Fragment) {
     "use strict";
 
     return Controller.extend("zmg.pro.exim.transactionalshippingbill.exim.controller.View1", {
         onInit: function () {
 
-        }
+        },
+        onSubScreenForLicense:function(){
+            var oView = this.getView();
+            if (!this._oFragmentavail) {
+              this._oFragmentavail = sap.ui.xmlfragment("zmg.pro.exim.transactionalshippingbill.exim.view.fragments.SubScreenForLicense", this);
+              oView.addDependent(this._oFragmentavail);
+            }
+            this._oFragmentavail.open();
+        },
+        availResourceClose: function () {
+            this._oFragmentavail.close();
+          }
     });
 });
